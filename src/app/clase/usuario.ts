2a2bd03b78285425eeaeaf963e5948aeba300c7b
Rolando Pedro Gabriel <136540189+scownsubi@users.noreply.github.com>
2023-06-16 16:13:25 -0600

    Update usuario.ts

--- src/app/clase/usuario.ts
+++ src/app/clase/usuario.ts
@@ -1,4 +1,6 @@
 export class Usuario {
+    // Los tipos de datos con mayúsculas son objetos
+    // Los tipos de datos con minúsuclas son primitivos
     id: Number = 0
     name: String = ""
     password: String = ""
